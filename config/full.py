373707150f46d932dcc57965153f7f1fdb2d36e7
jguinot <jul.guinot@gmail.com>
2023-10-10 11:58:41 +0100

    better way of dealing with loading global config

--- config/full.py
+++ config/full.py
@@ -6,13 +6,6 @@ from config.template import Config
 class GlobalConfig(Config):
     def __init__(self) -> None:
         super().__init__()
-        self.MTAT_config = MTATConfig()
-        self.preprocessing_config = PreprocessingConfig()
-        self.train_config = TrainConfig()
-
-    def make_global_config(self):
-        return {
-            "MTAT_config" : self.MTAT_config.to_dict(),
-            "preprocessing_config" : self.preprocessing_config.to_dict(),
-            "train_config" : self.train_config.to_dict()
-        }
+        self.MTAT_config = MTATConfig().to_dict()
+        self.preprocessing_config = PreprocessingConfig().to_dict()
+        self.train_config = TrainConfig().to_dict()
--- config/template.py
+++ config/template.py
@@ -1,14 +1,17 @@
 import yaml
 
+
 class Config:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, dict=None) -> None:
+        if dict is not None:
+            for k, v in dict.items():
+                setattr(self, k, v)
 
     def to_dict(self):
         return self.__dict__
 
-    def save(self,save_path):
-        
+    def save(self, save_path):
+
         with open(save_path, 'w') as outfile:
             yaml.dump(self.to_dict(), outfile, default_flow_style=False)
