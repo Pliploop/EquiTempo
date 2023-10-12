d86247c8563f680bdaf05e346da3581e0adebdc5
jguinot <jul.guinot@gmail.com>
2023-10-12 17:04:34 +0100

    conversion

--- src/utils/mp3_to_x.py
+++ src/utils/mp3_to_x.py
@@ -4,19 +4,27 @@ import scipy
 import numpy as np
 from tqdm import tqdm
 
-def convert_folder_to_extension(folder_path, extension=  'wav'):
+def convert_folder_to_extension(folder_path, new_folder = None, extension=  'wav'):
     for root, dirs, files in os.walk(folder_path):
+        if new_folder is None:
+            folder = root
+        else:
+            folder = root.replace(folder_path,new_folder)
+
         for name in tqdm(files):
             if ".mp3" in name:
                 try:
                     new_name = name[:-3]+extension
-                    if not os.path.exists(os.path.join(root,new_name)):
-                        y,sr =  librosa.load(os.path.join(root,name))
-                        
-                        if extension =='wav':
-                            scipy.io.wavfile.write(os.path.join(root,new_name), sr, y)
-                        if extension == 'npy':
-                            np.save(os.path.join(root,new_name), y)
+                    if not os.path.exists(os.path.join(folder,new_name)):
+                        print(f"saving {os.path.join(root,name)} to {os.path.join(folder,name)}")
+                        # y,sr =  librosa.load(os.path.join(root,name))
+                        # if not os.path.exists(folder):
+                        #     os.makedirs(folder)
+                        # if extension =='wav':
+                        #     scipy.io.wavfile.write(os.path.join(folder,new_name), sr, y)
+                        # if extension == 'npy':
+                        #     np.save(os.path.join(folder,new_name), y)
+                            
                 except:
                     print(os.path.join(root,name))
                     os.remove(os.path.join(root,name))
