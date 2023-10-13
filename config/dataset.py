81c3bf6a7ad9397fa4095d4a3d6d2a055898130e
Julien Paul Mateo Guinot <jpmg86@frank.eecs.qmul.ac.uk>
2023-10-13 17:53:26 +0100

    minor stuff

--- config/dataset.py
+++ config/dataset.py
@@ -5,8 +5,8 @@ class MTATConfig(Config):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        self.annotations_path = "data/MTAT_annotations.csv"
-        self.dir_path = "data/MTAT"
+        self.annotations_path = "/import/c4dm-datasets/MagnaTagATune/annotations_final.csv"
+        self.dir_path = "/import/research_c4dm/JulienMarcoChrisRMRI/MTAT_wav"
         self.batch_size = 16
         self.extension = 'wav'
 
