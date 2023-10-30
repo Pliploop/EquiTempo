c682fefd5eef89c03f0156d70d590c1f771eca44
chrispla <cplachouras@gmail.com>
2023-10-30 19:10:05 +0000

    rm test scripts

--- test_finetune.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
-
-import torch
-
-from src.training.train import Trainer
-from src.finetuning.finetuning import finetune
-
-finetune("filters_16_do_0.1_od_300/model_loss_0.0661_it_227856.pt")
